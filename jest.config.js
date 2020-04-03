2858e1c7600f40eb8b9c42af2cc027837f6fafff
Vincent Ballut <kaayru@gmail.com>
2020-04-03 14:52:41 +0200

    Fixes Jest config for root imports #19

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,10 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/file-mock.js',
+    '^fixtures(.*)$': '<rootDir>/__fixtures__$1',
+    '^mocks(.*)$': '<rootDir>/__mocks__$1',
+    '^src(.*)$': '<rootDir>/src$1',
+    '^utils(.*)$': '<rootDir>/utils$1',
   },
   testPathIgnorePatterns: ['node_modules', '\\.cache', '<rootDir>.*/public'],
   transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
